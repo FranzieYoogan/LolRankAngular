6e274d6d1661263c6a573516ff0818e945c1cdd8
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-03-30 12:45:03 -0300

    error

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { window } from 'rxjs';
 
 
 @Component({
@@ -21,25 +22,33 @@ export class SearchComponent {
   data2:any;
 
   getApi() {  
-    event?.preventDefault()
-    var search:any = document.getElementById('search');
+    
+    const search:any = document.getElementById('search');
 
     var apiKey = "RGAPI-d58c8b9e-c429-4f59-b71d-f55e6ef6fec7"
+    var summonerName:any = document.getElementById('summonerName')
     var tier:any = document.getElementById('tier')
     var rank:any = document.getElementById('rank')
     var wins:any = document.getElementById('wins')
     var losses:any = document.getElementById('losses')
+    var winRate:any = document.getElementById('winRate')
+    
 
-    var summonerName:any = document.getElementById('summonerName')
 
+
+    
+      
 
 
 this.http.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${search.value}?api_key=${apiKey}`).subscribe(res => {
+
 
     if(res) {
       const data = res
 
       this.data = data;
+
+   
 
     } 
 
@@ -49,59 +58,107 @@ this.http.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$
 
 
 
-    if(typeof this.data !== "undefined" ) {
 
-      const error:any = document.getElementById('error')
+
+
+  search.value = "Loading"
+
+
+  setTimeout(() => {
     
-      error.style.display = "none"
+    if(search.value == "Loading") {
+   
+
+      search.value = "ERROR"
+
+        
+      summonerName.innerHTML = ``
+      tier.innerHTML = ``
+      rank.innerHTML = ``
+      wins.innerHTML = ``
+      losses.innerHTML = ``
+      winRate.innerHTML = ``
+
+      console.clear()
+  
+   
+  }
+
+
+
+  }, 8000);
+
+
+
+ 
+
+
+
+
+    const error:any = document.getElementById('error')
+
+ 
+    
+    setTimeout(() => {
+
+
+
+    if(typeof this.data != "undefined" || typeof this.data != null) {
+
+
+
+
+
+  
+ 
   
       this.http.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${this.data.id}?api_key=${apiKey}`).subscribe(res2 => {
 
- 
-      this.data2 = res2;
+
+
+      console.clear()
   
-  
+
+      if(res2) {
+
+        this.data2 = res2;
+        search.value = this.data2[0].summonerName;
+
+      }
+
+
+
+   
         var totalGames:any = (this.data2[0].wins + this.data2[0].losses) * 100;
-        var winRate:any = document.getElementById('winRate')
+    
     
         var percentageW:any =  this.data2[0].wins/totalGames;
 
     
+     
   
-        summonerName.innerHTML = `Summoner Name: ${this.data2[0].summonerName}`
+        summonerName.innerHTML = `${this.data2[0].summonerName}`
         tier.innerHTML = `Tier: ${this.data2[0].tier}`
         rank.innerHTML = `Rank ${this.data2[0].rank}`
         wins.innerHTML = `Wins: ${this.data2[0].wins}`
         losses.innerHTML = `Losses ${this.data2[0].losses}`
         winRate.innerHTML = `Win Rate: ${Math.floor(percentageW * 10000)}`
+ 
       
-      
 
 
-   
      
-  
-     
-
 
   
     });
 
-  }  else {
-    const error:any = document.getElementById('error')
-    
-    error.style.display = "block"
+  }  
 
-
-  }
 
     
-  
+}, 6000);
 
-  
 
-   
-    
 
   }
 
